481af5aa96412918236d434ec815fddf57401ab9
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-05-13 21:01:23 +0800

    feat: more api endpoints

--- api.go
+++ api.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 )
 
+// Send Hypixel API HTTP Request
+// If you want bypass rate limit, use c.Rate.Reset()
 func (c *Client) Send(method string, head http.Header, path string, params *Params, payload ...byte) (*http.Response, error) {
 	if method == "" {
 		method = http.MethodGet
@@ -28,12 +30,16 @@ func (c *Client) Send(method string, head http.Header, path string, params *Para
 	if head != nil {
 		req.Header = head
 	}
-	c.Rate.WaitIfNeeded()
+	if c.Rate != nil {
+		c.Rate.WaitIfNeeded()
+	}
 	rsp, err := c.HTTP.Do(req)
 	if err != nil {
 		return nil, err
 	}
-	_ = c.Rate.UpdateRateLimitInfo(rsp.Header)
+	if c.Rate != nil {
+		_ = c.Rate.UpdateFromHeaders(rsp.Header)
+	}
 	return rsp, nil
 }
 
@@ -59,7 +65,49 @@ func (c *Client) Authentication(header ...http.Header) http.Header {
 // 403 Access is forbidden, usually due to an invalid API key being used.
 // 429 A request limit has been reached, usually this is due to the limit on the key being reached but can also be triggered by a global throttle.
 func (c *Client) GetPlayerData(uuid string) (*http.Response, error) {
-	return c.Send(http.MethodGet, c.Authentication(), "v2/player", &Params{
+	return c.Send(http.MethodGet, c.Authentication(), "player", &Params{
 		"uuid": uuid,
+	})
+}
+
+// GetRecentGames The recently played games of a specific player
+//
+// https://api.hypixel.net/#tag/Player-Data/paths/~1v2~1recentgames/get
+// 200 Get player's recent game
+// 400 Some data is missing, this is usually a field.
+// 403 Access is forbidden, usually due to an invalid API key being used.
+// 422 Some data provided is invalid.
+// 429 A request limit has been reached, usually this is due to the limit on the key being reached but can also be triggered by a global throttle.
+func (c *Client) GetRecentGames(uuid string) (*http.Response, error) {
+	return c.Send(http.MethodGet, c.Authentication(), "recentgames", &Params{
+		"uuid": uuid,
+	})
+}
+
+// GetStatus The current online status of a specific player
+//
+// https://api.hypixel.net/#tag/Player-Data/paths/~1v2~1status/get
+// 200 Get player status
+// 400 Some data is missing, this is usually a field.
+// 403 Access is forbidden, usually due to an invalid API key being used.
+// 429 A request limit has been reached, usually this is due to the limit on the key being reached but can also be triggered by a global throttle.
+func (c *Client) GetStatus(uuid string) (*http.Response, error) {
+	return c.Send(http.MethodGet, c.Authentication(), "status", &Params{
+		"uuid": uuid,
+	})
+}
+
+// GetGuild Retrieve a Guild by a player, id, or name
+//
+// https://api.hypixel.net/#tag/Player-Data/paths/~1v2~1guild/get
+// 200 Get guild information
+// 400 Some data is missing, this is usually a field.
+// 403 Access is forbidden, usually due to an invalid API key being used.
+// 429 A request limit has been reached, usually this is due to the limit on the key being reached but can also be triggered by a global throttle.
+func (c *Client) GetGuild(id, player, name string) (*http.Response, error) {
+	return c.Send(http.MethodGet, c.Authentication(), "guild", &Params{
+		"id":     id,
+		"player": player,
+		"name":   name,
 	})
 }
--- api_test.go
+++ api_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestClient_Authentication(t *testing.T) {
 	h := http.Header{}
-	if NewClient("test1").Authentication(h).Get("API-Key") != "test1" {
+	if NewClient("test1", nil).Authentication(h).Get("API-Key") != "test1" {
 		t.Errorf("expected 'test1', got %s", h.Get("API-Key"))
 	}
 }
