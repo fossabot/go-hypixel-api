58e704ef4621af536021a1114ebd85ffd3a6a4f6
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-05-13 23:09:51 +0800

    feat(api): add more test

--- api_test.go
+++ api_test.go
@@ -7,7 +7,12 @@ import (
 
 func TestClient_Authentication(t *testing.T) {
 	h := http.Header{}
-	if NewClient("test1", nil).AuthHeader(h).Get("API-Key") != "test1" {
+	h.Set("head", "value1")
+	c := NewClient("test1", nil)
+	if c.AuthHeader(h).Get("API-Key") != "test1" {
 		t.Errorf("expected 'test1', got %s", h.Get("API-Key"))
+	}
+	if h.Get("head") != "value1" {
+		t.Errorf("expected 'value1', got %s", h.Get("head"))
 	}
 }
